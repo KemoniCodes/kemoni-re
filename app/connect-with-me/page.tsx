d0f5d447b44aaf9299096a637e2c3e60a68a90c6
kemonicodes22 <kemoniw333@gmail.com>
2025-01-24 02:04:53 -0800

    digital biz card desktop + mobile

---
+++ app/connect-with-me/page.tsx
@@ -0,0 +1,86 @@
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+import Logo from "../../public/verticalLogo.svg";
+import BgImg from "../../public/bizCardImg.jpg";
+
+export default function ConnectWithMe() {
+  return (
+    <>
+      <div className='connectWithMe'>
+        <div
+          className='connectWithMeBgImg bg-cover -ml-8 pl-8 lg:h-[50vh] w-screen h-[24vh]'
+          style={{
+            backgroundImage: BgImg ? `url(${BgImg.src})` : "none",
+          }}
+        >
+          <div className='absolute inset-0 bg-black opacity-50 lg:h-[50vh] w-screen h-[24vh]' />
+        </div>
+        <Image
+          src={Logo}
+          alt='Kemoni Williams Real Estate Logo'
+          width={202.91}
+          height={110}
+          className='mx-auto relative -mt-8'
+        />
+
+        <div className='contactInfo mt-[76px]'>
+          <ul className='text-center list-none flex flex-col gap-8'>
+            <li>
+              <h3 className='normal-case '>Kemoni Williams</h3>
+              <p className=' text-shadowGrey text-[16px]'>
+                real estate specialist
+              </p>
+            </li>
+            <li>
+              <h3 className='normal-case '>
+                <Link
+                  className=''
+                  href={
+                    "https://mail.google.com/mail/?view=cm&fs=1&to=kemoni@kemoniwilliams.com"
+                  }
+                  target='#'
+                >
+                  kemoni@kemoniwilliams.com
+                </Link>
+              </h3>
+              <p className=' text-shadowGrey text-[16px]'>email</p>
+            </li>
+            <li>
+              <h3 className='normal-case '>
+                <Link className='' href={"tel:562-234-6847"}>
+                  562-234-6847
+                </Link>
+              </h3>
+              <p className=' text-shadowGrey text-[16px]'>phone</p>
+            </li>
+            <li>
+              <h3 className='normal-case '>
+                <Link
+                  className=''
+                  href={"https://kemoniwilliams.com"}
+                  target='#'
+                >
+                  kemoniwilliams.com
+                </Link>
+              </h3>
+              <p className=' text-shadowGrey text-[16px]'>website</p>
+            </li>
+            <li>
+              <h3 className='underline underline-[1px] leading-6'>
+                Access Your Complimentary Buyer’s Guide
+              </h3>
+            </li>
+          </ul>
+        </div>
+
+        <div className='tagline absolute lg:bottom-auto  bottom-0 -ml-8 w-full pb-5 lg:mt-20  mt-0'>
+          <h3 className=' text-center '>
+            curating homes.
+            <br /> curating lifestyles.
+          </h3>
+        </div>
+      </div>
+    </>
+  );
+}
