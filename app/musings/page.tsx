9c129ad6e55ded9bbdc09d4ee047f770a1df1c4c
kemonicodes22 <kemoniw333@gmail.com>
2025-02-20 02:13:09 -0800

    sticky scroll

--- app/musings/page.tsx
+++ app/musings/page.tsx
@@ -45,58 +45,57 @@ export default function Musings() {
       </div>
       {/* gap-56 */}
       <div className='blogInfoContainer flex mt-44 w-full gap-32'>
-        <div className='left grid grid-cols-4 gap-4 gap-x-3 h-fit flex-1'>
+        <div className='left grid grid-cols-4 gap-4 gap-x-3 h-fit flex-1 sticky top-24 self-start'>
           {blogData?.allFilters?.map((filter, index) => (
             <span className='h3 blogFilter transitionHover' key={index}>
               {filter}
             </span>
           ))}
         </div>
-        <div className='right flex-1'>
+        <div className='right flex-1 overflow-y-auto'>
           <div className='articlesContainer'>
             {blogData?.articles?.map((article, index) => (
-              <>
-                <Link
-                  href={`/musings/${article?.articleTitle
-                    ?.toLowerCase()
-                    .replace(/[^a-z0-9]+/g, "-")
-                    .replace(/^-+|-+$/g, "")
-                    .slice(0, 200)}`}
-                >
-                  <div className='article transitionHover' key={index}>
-                    <Image
-                      src={urlFor(article?.articleThumbnail)
-                        .width(670)
-                        .height(326)
-                        .url()}
-                      alt={`${article?.articleThumbnail?.alt}`}
-                      width={670}
-                      height={326}
-                      className='rounded-lg'
-                    />
-                    <div className='articleText mt-8 w-[80%]'>
-                      <h3 className='date mb-[10px]'>
-                        {formatDate(article?.articleDate ?? "")}
-                      </h3>
-                      <h2 className='leading-8'>{article?.articleTitle}</h2>
-                      <div className='filters flex gap-3 mt-4'>
-                        {article?.filters?.map((filter, index) => (
-                          <span className='h3 blogFilter !w-fit' key={index}>
-                            {filter}
-                          </span>
-                        ))}
-                      </div>
-                      <p className='summary mt-6'>
-                        {article?.articleText
-                          ?.find((paragraph) => paragraph.style === "normal")
-                          ?.children?.[0]?.text?.split(" ")
-                          ?.slice(0, 27)
-                          ?.join(" ") + "..."}
-                      </p>
+              <Link
+                href={`/musings/${article?.articleTitle
+                  ?.toLowerCase()
+                  .replace(/[^a-z0-9]+/g, "-")
+                  .replace(/^-+|-+$/g, "")
+                  .slice(0, 200)}`}
+                key={index}
+              >
+                <div className='article transitionHover mb-20'>
+                  <Image
+                    src={urlFor(article?.articleThumbnail)
+                      .width(670)
+                      .height(326)
+                      .url()}
+                    alt={`${article?.articleThumbnail?.alt}`}
+                    width={670}
+                    height={326}
+                    className='rounded-lg'
+                  />
+                  <div className='articleText mt-8 w-[80%]'>
+                    <h3 className='date mb-[10px]'>
+                      {formatDate(article?.articleDate ?? "")}
+                    </h3>
+                    <h2 className='leading-8'>{article?.articleTitle}</h2>
+                    <div className='filters flex gap-3 mt-4'>
+                      {article?.filters?.map((filter, index) => (
+                        <span className='h3 blogFilter !w-fit' key={index}>
+                          {filter}
+                        </span>
+                      ))}
                     </div>
+                    <p className='summary mt-6'>
+                      {article?.articleText
+                        ?.find((paragraph) => paragraph.style === "normal")
+                        ?.children?.[0]?.text?.split(" ")
+                        ?.slice(0, 27)
+                        ?.join(" ") + "..."}
+                    </p>
                   </div>
-                </Link>
-              </>
+                </div>
+              </Link>
             ))}
           </div>
         </div>
