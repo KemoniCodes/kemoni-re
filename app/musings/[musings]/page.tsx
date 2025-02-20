3cd9eaa03f7e20bb22962b105d3616e7b6dfdafc
kemonicodes22 <kemoniw333@gmail.com>
2025-02-20 00:07:37 -0800

    reccomended articles initial logic/styling

--- app/musings/[musings]/page.tsx
+++ app/musings/[musings]/page.tsx
@@ -2,8 +2,8 @@
 import React, { useState, useEffect } from "react";
 import { Blog } from "@/sanity/types";
 import { getBlog } from "@/sanity/sanity.query";
-// import Link from "next/link";
-// import Image from "next/image";
+import Link from "next/link";
+import Image from "next/image";
 import { urlFor } from "@/app/utils/imageUrl";
 import { usePathname } from "next/navigation";
 
@@ -51,15 +51,27 @@ export default function ArticlePage() {
     });
   };
 
-  const articleThumbnail = articleData?.articles?.map((article) => {
-    return article?.articleThumbnail?.asset?._ref
-      ? urlFor(article?.articleThumbnail).url()
-      : null;
+  const articleThumbnail = currentArticle?.articleThumbnail?.asset?._ref
+    ? urlFor(currentArticle?.articleThumbnail).url()
+    : null;
+
+  const currentTags = currentArticle?.filters?.map((tag) => {
+    return tag;
   });
+
+  const matchingArticles = articleData?.articles?.filter(
+    (article) =>
+      article !== currentArticle &&
+      article?.filters?.some((filter) => currentTags?.includes(filter))
+  );
+
+  const otherArticles =
+    articleData?.articles?.filter((article) => article !== currentArticle) ??
+    [];
 
   console.log(articleData);
 
-  if (!articleData) {
+  if (!articleThumbnail) {
     return <h2>Loading...</h2>;
   }
 
@@ -127,6 +139,118 @@ export default function ArticlePage() {
           })
           .filter(Boolean)}
       </div>
+
+      {(matchingArticles?.length ?? 0) > 0 ? (
+        <div className='relatedArticles relative '>
+          <span className='title transition-all duration-300 ease-in-out'>
+            <h2>featured</h2>
+            <h1>listings</h1>
+          </span>
+
+          <div className='relatedArticle pt-6 flex gap-5 w-full'>
+            {matchingArticles?.map((article, index) => (
+              <Link
+                href={`/musings/${article?.articleTitle
+                  ?.toLowerCase()
+                  .replace(/[^a-z0-9]+/g, "-")
+                  .replace(/^-+|-+$/g, "")
+                  .slice(0, 200)}`}
+              >
+                <div
+                  className='article flex flex-col flex-shrink-0 max-w-[324px] transitionHover'
+                  key={index}
+                >
+                  <div
+                    className='articleThumbnail bg-cover relative -z-10 rounded-lg h-[324px]'
+                    style={{
+                      backgroundImage: article?.articleThumbnail?.asset?._ref
+                        ? `url(${urlFor(article.articleThumbnail).url()})`
+                        : article?.articleThumbnail
+                          ? `url(${article.articleThumbnail})`
+                          : "none",
+                    }}
+                  >
+                    <div className='absolute inset-0 bg-black opacity-50' />
+                    <div className='thumbnailFilter absolute top-0 p-4'>
+                      <div className='filters flex gap-3'>
+                        {article?.filters?.map((filter, index) => (
+                          <span className='h3 blogFilter !w-fit' key={index}>
+                            {filter}
+                          </span>
+                        ))}
+                      </div>
+                    </div>
+                  </div>
+
+                  <div className='homeInfo mt-5'>
+                    <h3>
+                      <b>{article?.articleTitle}</b>
+                    </h3>
+                    <h3 className='mt-[6px]'>
+                      {formatDate(article?.articleDate ?? "")}
+                    </h3>
+                  </div>
+                </div>
+              </Link>
+            ))}
+          </div>
+        </div>
+      ) : (
+        <div className='otherArticles relative '>
+          <span className='title transition-all duration-300 ease-in-out'>
+            <h2>read</h2>
+            <h1>more</h1>
+          </span>
+
+          <div className='otherArticle pt-6 flex gap-5 w-full'>
+            {otherArticles?.map((article, index) => (
+              <Link
+                href={`/musings/${article?.articleTitle
+                  ?.toLowerCase()
+                  .replace(/[^a-z0-9]+/g, "-")
+                  .replace(/^-+|-+$/g, "")
+                  .slice(0, 200)}`}
+              >
+                <div
+                  className='article flex flex-col flex-shrink-0 max-w-[324px] transitionHover'
+                  key={index}
+                >
+                  <div
+                    className='articleThumbnail bg-cover relative -z-10 rounded-lg h-[324px]'
+                    style={{
+                      backgroundImage: article?.articleThumbnail?.asset?._ref
+                        ? `url(${urlFor(article.articleThumbnail).url()})`
+                        : article?.articleThumbnail
+                          ? `url(${article.articleThumbnail})`
+                          : "none",
+                    }}
+                  >
+                    <div className='absolute inset-0 bg-black opacity-50' />
+                    <div className='thumbnailFilter absolute top-0 p-4'>
+                      <div className='filters flex gap-3'>
+                        {article?.filters?.map((filter, index) => (
+                          <span className='h3 blogFilter !w-fit' key={index}>
+                            {filter}
+                          </span>
+                        ))}
+                      </div>
+                    </div>
+                  </div>
+
+                  <div className='homeInfo mt-5'>
+                    <h3>
+                      <b>{article?.articleTitle}</b>
+                    </h3>
+                    <h3 className='mt-[6px]'>
+                      {formatDate(article?.articleDate ?? "")}
+                    </h3>
+                  </div>
+                </div>
+              </Link>
+            ))}
+          </div>
+        </div>
+      )}
     </div>
   );
 }
